d4f715be6106ae657a85f3ebf548fde49406588b
William Nkosi <williamnkosi@Williams-MacBook-Pro.local>
2025-05-08 21:48:57 -0400

    Created the google_maps_service

--- src/google_maps_service/google_maps_service.module.ts
+++ src/google_maps_service/google_maps_service.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GoogleMapsServiceService } from './google_maps_service.service';
+import { GoogleMapsService } from './google_maps_service.service';
 
 @Module({
-  providers: [GoogleMapsServiceService]
+  providers: [GoogleMapsService],
+  exports: [GoogleMapsService],
 })
 export class GoogleMapsServiceModule {}
