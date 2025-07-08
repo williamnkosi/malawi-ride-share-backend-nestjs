76fc7962066433748c1836fc15a2ecbd2ebb42ae
William Nkosi <williamnkosi@mac.mynetworksettings.com>
2025-07-07 23:59:53 -0400

    deleted the User Module, Rider Module and Driver Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+
 import { NotificationsModule } from './notifications/notifications.module';
 
-import { LocationModule } from './location/location.module';
-import { PaymentsModule } from './payments/payments.module';
-import { DriversModule } from './driver/driver.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { ConfigModule } from '@nestjs/config';
 import { TestingModule } from './testing/testing.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RiderModule } from './riders/riders.module';
-//import { RidersController } from './riders/riders.controller';
 
 import { RiderReviewModule } from './rider_review/rider_review.module';
 import { DriverReviewModule } from './driver_review/driver_review.module';
 import { UserDeviceModule } from './user_device/user_device.module';
-import { TripModule } from './trip/trip.module';
-import { RiderController } from './riders/riders.controller';
-import { DriverController } from './driver/driver.controller';
-import { TripController } from './trip/trip.controller';
+
 import { GoogleMapsServiceModule } from './google_maps_service/google_maps_service.module';
-import { DriverLocationTrackingModule } from './tracking/driver_location_tracking/driver_location_tracking.module';
-import { TrackingModule } from './tracking/tracking.module';
 import { LocationTrackingModule } from './location_tracking/location_tracking.module';
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -41,24 +31,19 @@ const isProd = process.env.NODE_ENV === 'production';
       //dropSchema: !isProd, // Drop schema in development
     }),
     AuthModule,
-    UsersModule,
+
     NotificationsModule,
-    LocationModule,
-    PaymentsModule,
-    DriversModule,
+
     FirebaseModule,
     ...(!isProd ? [TestingModule] : []),
-    RiderModule,
+
     RiderReviewModule,
     DriverReviewModule,
     UserDeviceModule,
-    TripModule,
     GoogleMapsServiceModule,
-    DriverLocationTrackingModule,
-    TrackingModule,
     LocationTrackingModule,
   ],
-  controllers: [RiderController, DriverController, TripController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
