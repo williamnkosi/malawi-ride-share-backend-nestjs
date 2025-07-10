ffe781a93994277b7e7a42a5bca88ada26a542a3
William Nkosi <williamnkosi@mac.mynetworksettings.com>
2025-07-10 00:47:39 -0400

    Fixed modules

--- src/users/drivers/drivers.module.ts
+++ src/users/drivers/drivers.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DriverEntity } from './driver_entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([DriverEntity])],
   controllers: [DriversController],
-  providers: [DriversService]
+  providers: [DriversService],
 })
 export class DriversModule {}
--- src/users/riders/riders.module.ts
+++ src/users/riders/riders.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RidersService } from './riders.service';
 import { RidersController } from './riders.controller';
+import { RiderEntity } from './rider_entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RiderEntity])],
   providers: [RidersService],
   controllers: [RidersController],
 })
