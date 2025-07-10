a285c9e63c72bc15ff97e263bad80e60d5235451
William Nkosi <williamnkosi@mac.mynetworksettings.com>
2025-07-10 00:32:37 -0400

    Added controller to create a driver

--- src/users/drivers/drivers.controller.ts
+++ src/users/drivers/drivers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { DriversService } from './drivers.service';
+import { CreateDriverDto } from './drivers.dto';
 
 @Controller('drivers')
-export class DriversController {}
+export class DriversController {
+  constructor(private readonly driversService: DriversService) {}
+  @Post()
+  async createDriver(@Body() createDriverDto: CreateDriverDto) {
+    return await this.driversService.create(createDriverDto);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -24,26 +24,26 @@ export abstract class UsersService<T extends UserEntity> {
       throw new CustomError('Error finding user', 500);
     }
   }
-  async findById(id: string): Promise<T | null> {
+  async findById(firebaseId: string): Promise<T | null> {
     try {
       return await this.repository.findOne({
-        where: { id } as FindOptionsWhere<T>,
+        where: { firebaseId } as FindOptionsWhere<T>,
       });
     } catch {
       throw new CustomError('Error finding user', 500);
     }
   }
-  async update(id: string, data: Partial<T>): Promise<T | null> {
+  async update(firebaseId: string, data: Partial<T>): Promise<T | null> {
     try {
-      await this.repository.update(id, data as any);
-      return this.findById(id);
+      await this.repository.update(firebaseId, data as any);
+      return this.findById(firebaseId);
     } catch {
       throw new CustomError('Error updating user', 500);
     }
   }
-  async remove(id: string): Promise<void> {
+  async remove(firebaseId: string): Promise<void> {
     try {
-      await this.repository.delete(id);
+      await this.repository.delete(firebaseId);
     } catch {
       throw new CustomError('Error deleting user', 500);
     }
